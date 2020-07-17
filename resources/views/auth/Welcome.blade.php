2173e2e283de06ef52fa6c38c48fcfd2a38c33e7
bellps <isabella020240@hotmail.com>
2020-07-17 14:10:32 -0300

    Adicionando textos na home

--- resources/views/auth/Welcome.blade.php
+++ resources/views/auth/Welcome.blade.php
@@ -18,19 +18,20 @@
             <h1><i class="fas fa-address-card"></i> Sobre nós</h1>
             <div class="row content" data-aos="fade-left">
                 <div class="col-md-4">
-                    <h5>Alguma coisa</h5>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet tincidunt magna. Nam dictum vehicula lectus, id laoreet turpis placerat sed. Nullam ac metus diam. Fusce tincidunt molestie diam sit amet molestie. Sed quis imperdiet ex. Proin lobortis mauris sit amet ultricies faucibus. Nullam at tortor eu turpis pulvinar porttitor et sit amet ante. Aenean consectetur accumsan leo eu ultrices. Nulla tincidunt id ligula nec pretium. Nullam quis eros id orci elementum pretium ac eget sapien. In hac habitasse platea dictumst. Nunc dignissim, odio nec consequat malesuada, dolor mauris pharetra nibh, at eleifend leo quam et purus. Nunc quis volutpat turpis. Praesent porta consectetur nisl, nec porta nibh mattis vel. Proin quis placerat massa, vitae efficitur 
+                    <h5>O que é? </h5>
+                    <p>A <em>S.O.S.: Estudos em Casa</em> é uma plataforma criada para conectar os alunos que possuem dúvidas em alguns assuntos com estudantes que já o dominam.</p>
                 </div>
                 <div class="col-md-4">
-                    <h5>Alguma coisa</h5>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet tincidunt magna. Nam dictum vehicula lectus, id laoreet turpis placerat sed. Nullam ac metus diam. Fusce tincidunt molestie diam sit amet molestie. Sed quis imperdiet ex. Proin lobortis mauris sit amet ultricies faucibus. Nullam at tortor eu turpis pulvinar porttitor et sit amet ante. Aenean consectetur accumsan leo eu ultrices. Nulla tincidunt id ligula nec pretium. Nullam quis eros id orci elementum pretium ac eget sapien. In hac habitasse platea dictumst. Nunc dignissim, odio nec consequat malesuada, dolor mauris pharetra nibh, at eleifend leo quam et purus. Nunc quis volutpat turpis. Praesent porta consectetur nisl, nec porta nibh mattis vel. Proin quis placerat massa, vitae efficitur quam.
-                    
+                    <h5>Como funciona?</h5>
+                    <p>A ideia é simples! Você se cadastra, colocando o link de suas redes sociais para as pessoas te encontrarem. Depois acrescenta alguns conteúdos que você conhece bem a ponto de conseguir ensinar os outros. Feito isso, você pode clicar em algum assunto que tem dúvidas e descobrir quem pode te ajudar, entrar em contato com a pessoa e assim se ajudarem.           </p>
                 </div>
                 <div class="col-md-4">
-                    <h5>Alguma coisa</h5>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet tincidunt magna. Nam dictum vehicula lectus, id laoreet turpis placerat sed. Nullam ac metus diam. Fusce tincidunt molestie diam sit amet molestie. Sed quis imperdiet ex. Proin lobortis mauris sit amet ultricies faucibus. Nullam at tortor eu turpis pulvinar porttitor et sit amet ante. Aenean consectetur accumsan leo eu ultrices. Nulla tincidunt id ligula nec pretium. Nullam quis eros id orci elementum pretium ac eget sapien. In hac habitasse platea dictumst. Nunc dignissim, odio nec consequat malesuada, dolor mauris pharetra nibh, at eleifend leo quam et purus. Nunc quis volutpat turpis. Praesent porta consectetur nisl, nec porta nibh mattis vel. Proin quis placerat massa, vitae efficitur quam.
-                    
+                    <h5>Por que foi criado?</h5>
+                    <p>A iniciativa foi inspirada na teoria de William Glasser (1925-2013), um psiquiatra americano, que diz que 95% do conhecimento é aprendido quando temos que ensinar alguém. Você pode ler um pouco mais sobre o estudo <a href="http://www.institutopuruna.com.br/como-aprendemos-a-teoria-da-escolha-de-william-glasser/" target="none">aqui</a>.</p>
                 </div>
+            </div>
+            <div class="text-center">
+                 <img src="{{ asset('/imgs/livros.png') }}" width="100%" alt="">
             </div>
         </div>
     </div>
@@ -39,22 +40,22 @@
         <div class="container" id="contato">
             <h1><i class="fas fa-comments"></i> Contato</h1>
             <div data-aos="fade-left">
-                <p>Siga a <i>EE "Padre Armani"</i> nas redes sociais: <a target="none" href="https://www.facebook.com/pe.armani.3/"><span class="fab fa-facebook-square"></span></a>   <a href="" target="none"><span class="fab fa-instagram"> </span></a></p> 
+                <p>Siga a <em>EE "Padre Armani"</em> nas redes sociais: <a target="none" href="https://www.facebook.com/pe.armani.3/"><span class="fab fa-facebook-square"></span></a>   <a href="" target="none"><span class="fab fa-instagram"> </span></a></p> 
                 <h4>Dúvidas ou sujestões? Nos mande um email!</h4>
                 <form action="https://formspree.io/sosestudosemcasa@gmail.com" method="POST">
                     <div class="form-row">
                         <div class="form-group col-md-6">
                             <label for="nome">Nome</label>
-                            <input type="text" name="nome" id="nome" class="form-control">
+                            <input type="text" name="nome" id="nome" class="form-control" required>
                         </div>
                         <div class="form-group col-md-6">
-                            <label for="email"> Email</label>
-                            <input type="email" name="email" id="email" class="form-control">
+                            <label for="email">Email</label>
+                            <input type="email" name="email" id="email" class="form-control" required>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="comments">Comentários</label>
-                        <textarea name="comments" id="comments" cols="30" rows="5" class="form-control"></textarea>
+                        <textarea name="comments" id="comments" cols="30" rows="5" class="form-control" required></textarea>
                     </div>
                     <div class="text-right">
                         <button type="submit" class="btn pink">Enviar</button>
