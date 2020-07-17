a652c7ed33213c89666c0d1d384997b21dea8b05
lucas delfini <delfini.lucas@hotmail.com>
2020-07-16 21:10:57 -0300

    add todas as rotas e seus cadastros

--- app/Http/Controllers/matematica.php
+++ app/Http/Controllers/matematica.php
@@ -11,11 +11,103 @@ class matematica extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function conjunto()
     {
         $posts = Post::where('matematica', '=', 'Conjuntos numéricos')->get();
         return view('Matérias/Conjuntos', compact([ 'posts']));
     }
+
+    public function Regularidades()
+    {
+        $posts = Post::where('matematica', '=', 'Regularidades numéricas')->get();
+        return view('Matérias/regularidade', compact([ 'posts']));
+    }
+
+    public function Progressoes()
+    {
+        $posts = Post::where('matematica', '=', 'Progressões aritméticas  e progressões geométricas')->get();
+        return view('Matérias/Progressoes', compact([ 'posts']));
+    }
+
+    public function duasgrandezas()
+    {
+        $posts = Post::where('matematica', '=', 'Relação entre duas grandezas')->get();
+        return view('Matérias/duasgrandezas', compact([ 'posts']));
+    }
+
+    public function Proporcionalidades()
+    {
+        $posts = Post::where('matematica', '=', 'Proporcionalidades')->get();
+        return view('Matérias/Proporcionalidades', compact([ 'posts']));
+    }
+
+    public function Funcao_de_1_grau()
+    {
+        $posts = Post::where('matematica', '=', 'Função de 1º grau')->get();
+        return view('Matérias/Funcao_de_1_grau', compact([ 'posts']));
+    }
+
+    public function Funcao_de_2_grau()
+    {
+        $posts = Post::where('matematica', '=', 'Função de 2º grau')->get();
+        return view('Matérias/Funcao_de_2_grau', compact([ 'posts']));
+    }
+
+    public function Crescimento()
+    {
+        $posts = Post::where('matematica', '=', 'Crescimento')->get();
+        return view('Matérias/Crescimento', compact([ 'posts']));
+    }
+
+    public function Funcao_exponencial()
+    {
+        $posts = Post::where('matematica', '=', 'Função exponencial')->get();
+        return view('Matérias/Funcao_exponencial', compact([ 'posts']));
+    }
+
+    public function inequações()
+    {
+        $posts = Post::where('matematica', '=', 'inequações')->get();
+        return view('Matérias/inequações', compact([ 'posts']));
+    }
+
+    public function Logaritmos()
+    {
+        $posts = Post::where('matematica', '=', 'Logaritmos')->get();
+        return view('Matérias/Logaritmos', compact([ 'posts']));
+    }
+
+    public function Função_logarítmica()
+    {
+        $posts = Post::where('matematica', '=', 'Função logarítmica')->get();
+        return view('Matérias/Função_logarítmica', compact([ 'posts']));
+    }
+
+    public function trigonométricas()
+    {
+        $posts = Post::where('matematica', '=', 'Razões trigonométricas nos triângulos retângulos')->get();
+        return view('Matérias/trigonométricas', compact([ 'posts']));
+    }
+
+    public function Polígonos_regulares()
+    {
+        $posts = Post::where('matematica', '=', 'Polígonos regulares')->get();
+        return view('Matérias/Polígonos_regulares', compact([ 'posts']));
+    }
+    
+    public function triângulos_não_retângulos()
+    {
+        $posts = Post::where('matematica', '=', 'Resolução de triângulos não retângulos')->get();
+        return view('Matérias/triângulos_não_retângulos', compact([ 'posts']));
+    }
+    
+    public function seus_cadastros()
+    {
+        $posts = Post::all();
+        return view('seus_cadastros', compact([ 'posts']));
+    }
+    
+    
 
     /**
      * Show the form for creating a new resource.
---
+++ resources/views/Matérias/Função_logarítmica.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.side-nav')
+
+@section('materia')
+        <h3>Função logarítmica</h3>
+        <hr>
+        <div class="row">
+            @foreach($posts as $post)
+                <div class="card" id="cartao">
+                    <div class="card-body">
+                        <h4 class="card-title" title="{{ $post->name}}">
+                            <i>{{ $post->name}} </i>
+                        </h4>
+                        <h6 class="card-subtitle mb-2 text-muted">{{$post->serie}}</h6>
+                        <h6><em>É ótimo(a) com:</em></h6>
+                        <p class="card-text">{{$post->matematica}}</p>
+                        <h6 class="text-right">
+                            <a href="{{$post->face}}" target="none"> <span class="fab fa-facebook-square"> </span></a>
+                            <a href="{{$post->insta}}" target="none"> <span class="fab fa-instagram"> </span></a> 
+                        </h6>
+                        @if (Auth::user()->name == $post->name)
+                            <div class="text-right">
+                                <form method='post' action="/post{{ $post->id}}">
+                                    @csrf
+                                    <input type="hidden" name="_method" value="delete">
+                                    <button type="submit" class="btn btn-sm btn-outline-danger">Apagar</button>
+                                </form>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+            @endforeach  
+        </div>
+@endsection
